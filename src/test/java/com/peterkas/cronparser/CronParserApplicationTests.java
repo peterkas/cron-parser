88b976e47eba651944d4e1cdadb4b7ee1c67d4cd
Pedro Casis <pekasios@gmail.com>
2022-02-06 12:30:53 +0000

    initial commit

---
+++ src/test/java/com/peterkas/cronparser/CronParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterkas.cronparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
