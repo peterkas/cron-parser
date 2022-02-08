33abd64990f713c57b5e04b50a0b5fdb8bbe63f8
Pedro Casis <pekasios@gmail.com>
2022-02-08 20:21:40 +0000

    Minor format fix

--- src/test/java/com/peterkas/cronparser/cron/CronBaseTest.java
+++ src/test/java/com/peterkas/cronparser/cron/CronBaseTest.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 abstract class CronBaseTest {
+
     protected List<Integer> convertCronValuesToInt(List<CronValue> cronValues) {
         List<Integer> intValues = new ArrayList<>(cronValues.size());
         cronValues.forEach(x -> intValues.add(x.getVal()));
         return intValues;
     }
+
 }
--- src/test/java/com/peterkas/cronparser/cron/CronExpressionTest.java
+++ src/test/java/com/peterkas/cronparser/cron/CronExpressionTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class CronExpressionTest extends CronBaseTest{
+public class CronExpressionTest extends CronBaseTest {
 
     @Test
     public void emptyExpressionThrowsException() {
